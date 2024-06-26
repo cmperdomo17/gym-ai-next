c4a0cda7b9d5c1b11d0812b316aa289b4278ee96
Carlos Mario Perdomo Ramos <cmperdomo@unicauca.edu.co>
2024-06-25 23:18:41 -0500

    feat: Add favicon icon to metadata and update navbar styling

--- src/app/lib/metadata.ts
+++ src/app/lib/metadata.ts
@@ -4,4 +4,7 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
     title: "GymAi App - Next.js",
     description: "Project GymAi App create with Next.js",
+    icons: {
+        icon: "/favicon.ico",
+    }
 };
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,6 +1,7 @@
 export default function NavBar() {
+
     return (
-        <nav className="flex bg-neutral-950 justify-center gap-7 lg:gap-20 top-0 fixed w-full text-lg text-white font-semibold p-3"> 
+        <nav className="flex bg-neutral-950 justify-center gap-7 lg:gap-20 top-0 fixed w-full text-sm border-b border-white md:border-none lg:text-base text-white font-semibold p-3"> 
             <a className="hover:underline" href="/">Home</a>
             <a className="hover:underline" href="/about">About Us</a>
             <a className="hover:underline" href="/login">Login</a>
