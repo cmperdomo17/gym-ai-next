ae34d75918cdf6efa378823cc0d9dd4d02b48e71
Carlos Mario Perdomo Ramos <cmperdomo@unicauca.edu.co>
2024-06-29 20:05:27 -0500

    feat: Update home page layout and styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <div className="flex flex-col gap-10 justify-center items-center">
+      <div className="flex flex-col gap-10 justify-center items-center text-center mx-10 md:mx-0">
         <h1 className="text-3xl font-bold">
           Welcome to GymAi
         </h1>
