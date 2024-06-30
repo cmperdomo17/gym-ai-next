3fc0781f6440738586e7d3c95041c23826e7f4c9
Carlos Mario Perdomo Ramos <cmperdomo@unicauca.edu.co>
2024-06-29 20:00:02 -0500

    feat: Update home page content and styles
    - Added welcome message and user name display to the home page.
    - Updated the styling of the home page to improve the layout and readability.

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,8 +1,9 @@
-
 export default function Home() {
     return (
         <div className="flex flex-col gap-10 max-w-96 items-center ">
             <h1 className="text-3xl font-bold">Home</h1>
+            <p className="text-lg">Welcome user: <span className="font-bold">User</span></p>
+            <p className="text-lg">Are you ready to start your fitness journey?</p>
         </div>
-    )
+    );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,14 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl font-bold">
-        Welcome to GymAi
-      </h1>
+      <div className="flex flex-col gap-10 justify-center items-center">
+        <h1 className="text-3xl font-bold">
+          Welcome to GymAi
+        </h1>
+        <p className="text-lg">
+          <strong>GymAi:</strong> Your Personalized Fitness Trainer with <strong>Ai</strong>
+        </p>
+      </div>
       {/* <Image
         src={"/logo.png"}
         alt="GymAi Logo"
